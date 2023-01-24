ae4a72983b3092068fd659a0d519b986a23116e2
Aleksandr Altynov <altynov.00@gmail.com>
2023-01-24 22:08:06 +0800

    BI done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React, {useEffect, useState} from 'react';
-import AppHeader from './components/app-header/app-header.jsx'
-import BurgerIngredients from './components/burger-ingredients/burger-ingredients.jsx'
-import style from './App.module.css';
+import React, { useEffect, useState } from 'react';
+import AppHeader from './components/app-header/app-header';
+import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
+import style from './App.module.css'
 
 function App() {
-  return (
-    <>
-    <AppHeader />
-    <BurgerIngredients />
-    </>
-  );
+  const [cart, setCart] = useState([]);
+  const [isFetching,setIsFetching] = useState(true)
+  const [state, setState] = useState([]);
+  const dataUrl = 'https://norma.nomoreparties.space/api/ingredients'
+    
+    useEffect(() => {
+      fetch(dataUrl)
+      .then(res=>res.json())
+      .then(res=>{
+        setState(res['data'])
+        setIsFetching(false)
+      })
+    }, [])
+    
+    return (
+      !isFetching ? 
+      <>
+        <AppHeader />
+          <main className={style.main}>
+            <BurgerIngredients data={state} setCart={setCart}/>
+          </main>
+      </> : <div></div>
+    );
 }
 
 export default App;
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ElementConstructor from './element-constructor/element-constructor.jsx'
 import SwitchPanel from "./switch-panel/switch-panel";
 import style from './burger-ingredients.module.css'
 
@@ -8,7 +9,42 @@ const BurgerIngredients = (props) => {
             <p className={`text text_type_main-large ${style.title}`}>Соберите бургер</p>
             <div><SwitchPanel/></div>
             <div className={style.sectionList}>
-                
+                <h2>Булки</h2>
+                <div className={style.sectionListItem}>
+                    {props.data.map( i => {
+                        if(i["type"] === 'bun')  return <ElementConstructor 
+                        key={i["_id"]} 
+                        img={i["image"]} 
+                        price={i["price"]} 
+                        name={i["name"]}
+                        />
+                        else return null
+                    })}
+                </div>
+                <h2>Соусы</h2>
+                <div className={style.sectionListItem}>
+                    {props.data.map( i => {
+                        if(i["type"] === 'sauce')  return <ElementConstructor 
+                        key={i["_id"]} 
+                        img={i["image"]} 
+                        price={i["price"]} 
+                        name={i["name"]}
+                        />
+                        else return null
+                    })}
+                </div>
+                <h2>Начинки</h2>
+                <div className={style.sectionListItem}>
+                    {props.data.map( i => {
+                        if(i["type"] === 'main')  return <ElementConstructor 
+                        key={i["_id"]} 
+                        img={i["image"]} 
+                        price={i["price"]} 
+                        name={i["name"]}
+                        />
+                        else return null
+                    })}
+                </div>
             </div>
         </section>
     )
